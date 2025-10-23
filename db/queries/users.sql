dae09f00cf7d890e51d3a40b8eb4f05ed75ef8e5
Jack Cordery <jack.redolf@gmail.com>
2025-10-23 09:28:33 +0100

    updated db migrations and queries to account for session based auth

---
+++ db/queries/users.sql
@@ -0,0 +1,130 @@
+-- name: CreateUser :one 
+INSERT INTO
+  users (name, surname, email)
+VALUES
+  ($1, $2, $3)
+RETURNING
+  id;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+  id = $1,
+  name = $2,
+  surname = $3,
+  email = $4,
+  created_at = DEFAULT,
+  last_login = DEFAULT
+WHERE
+  id = $1
+RETURNING
+  id;
+
+-- name: DeleteUser :one
+DELETE FROM users
+WHERE
+  id = $1
+RETURNING
+  id;
+
+-- name: GetUserById :one
+SELECT
+  *
+FROM
+  users
+WHERE
+  id = $1
+LIMIT
+  1;
+
+-- name: GetUserByEmail :one
+SELECT
+  *
+FROM
+  users
+WHERE
+  email = $1
+LIMIT
+  1;
+
+-- name: GetEmployeeById :one
+SELECT
+  *
+FROM
+  employees
+WHERE
+  id = $1
+LIMIT
+  1;
+
+-- name: GetAllEmployees :many
+SELECT
+  *
+FROM
+  employees;
+
+-- name: GetSessionByToken :one
+SELECT
+  *
+FROM
+  sessions
+WHERE
+  session_token = $1;
+
+-- name: GetLatestSession :one
+SELECT
+  *
+FROM
+  sessions
+WHERE
+  user_id = $1 -- Replace 123 with the specific user ID
+ORDER BY
+  created_at DESC
+LIMIT
+  1;
+
+-- name: CreateSession :one
+INSERT INTO
+  sessions (user_id, session_token, expires_at)
+VALUES
+  ($1, $2, $3)
+RETURNING
+  id;
+
+-- name: DeleteSessionByToken :exec
+DELETE FROM sessions
+WHERE
+  session_token = $1;
+
+-- name: DeleteSessionsByUserID :exec
+DELETE FROM sessions
+WHERE
+  user_id = $1;
+
+-- name: DeleteExpiredSessions :exec
+DELETE FROM sessions
+WHERE
+  expires_at < CURRENT_TIMESTAMP;
+
+-- name: AssignRoleToUser :exec
+INSERT INTO
+  user_roles (user_id, role_id)
+VALUES
+  ($1, $2);
+
+-- name: GetRolesForUser :many
+SELECT
+  r.name
+FROM
+  roles r
+  JOIN user_roles ur ON ur.role_id = r.id
+WHERE
+  ur.user_id = $1;
+
+-- name: GetRoleByName :one
+SELECT
+  id
+FROM
+  roles
+WHERE
+  name = $1;

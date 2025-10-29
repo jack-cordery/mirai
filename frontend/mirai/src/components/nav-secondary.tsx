5c895ef4388398421d027b5cb9db3ecf4e2b08d0
Jack Cordery <jack.redolf@gmail.com>
2025-10-29 09:30:42 +0000

    removal of debug admin button

--- frontend/mirai/src/components/nav-secondary.tsx
+++ frontend/mirai/src/components/nav-secondary.tsx
@@ -33,7 +33,6 @@ export function NavSecondary({
                                         {items.map((item) => (
                                                 <SidebarMenuItem key={item.title}>
                                                         <SidebarMenuButton
-
                                                                 className={`${page == item.url ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear" : ""}`}
                                                                 asChild>
                                                                 <a href={item.url}>
--- frontend/mirai/src/pages/booking.tsx
+++ frontend/mirai/src/pages/booking.tsx
@@ -1,20 +1,10 @@
 import BookingCalendar from "@/components/booking-calendar";
-import { Button } from "@/components/ui/button";
 import { SparklesCore } from "@/components/ui/sparkles";
-import { useNavigate } from "react-router-dom";
 
 export default function Booking() {
-        const navigate = useNavigate();
         return (
                 <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
-                        {/* Top-left button */}
-                        <Button
-                                className="absolute top-4 left-4 z-30 px-3 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                                onClick={() => navigate("/create")}>
-                                Admin
-                        </Button>
 
-                        {/* Sparkles background */}
                         <div className="w-full absolute inset-0 h-screen">
                                 <SparklesCore
                                         id="tsparticles-calendar"
@@ -27,7 +17,6 @@ export default function Booking() {
                                 />
                         </div>
 
-                        {/* Calendar */}
                         <div className="relative z-20">
                                 <BookingCalendar />
                         </div>

05b3540f6af951d5e3606d81ad136a542a74b74a
Jack Cordery <jack.redolf@gmail.com>
2025-11-01 20:15:59 +0000

    booking endpoints and types

--- frontend/mirai/src/api/bookings.ts
+++ frontend/mirai/src/api/bookings.ts
@@ -1,31 +1,74 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
-//
-
-export async function postBookingType(postRequest: {
-  title: string;
-  description: string;
-  fixed: boolean;
-  cost: number;
+export async function postBooking(postRequest: {
+  user_id: number;
+  availability_slots: number[];
+  type_id: number;
+  notes: string;
 }) {
-  const res = await fetch(`${apiUrl}/booking_type`, {
+  const res = await fetch(`${apiUrl}/booking`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(postRequest),
   });
 
   if (!res.ok) {
-    throw new Error(`creating booking type failed with ${res.status}`);
+    throw new Error(`creating booking failed with ${res.status}`);
   }
   return res.json();
 }
 
-// get user by email and return details
-export async function getAllBookingTypes() {
-  const res = await fetch(`${apiUrl}/booking_type/`);
+export type PostBookingResponse = {
+  booking_id: number;
+};
+
+export async function getBooking(getRequest: { booking_id: number }) {
+  const res = await fetch(`${apiUrl}/booking/${getRequest.booking_id}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
 
   if (!res.ok) {
-    throw new Error(`Get all booking types failed with ${res.status}`);
+    throw new Error(`get booking failed with ${res.status}`);
   }
   return res.json();
 }
+
+export type GetBookingResponse = {
+  booking_id: number;
+  user_id: number;
+  type_id: number;
+  paid: boolean;
+  cost: number;
+  notes: string;
+  slot_ids: number[];
+  created_at: string;
+  last_edited: string;
+};
+
+export async function getAllBookings() {
+  const res = await fetch(`${apiUrl}/booking`);
+
+  if (!res.ok) {
+    throw new Error(`get all bookings failed with ${res.status}`);
+  }
+  return res.json();
+}
+
+export type GetAllBookingsResponse = {
+  id: number;
+  user_id: number;
+  user_name: string;
+  user_surname: string;
+  user_email: string;
+  user_last_login: string;
+  type_id: number;
+  type_title: string;
+  paid: boolean;
+  cost: number;
+  notes: string;
+  created_at: string;
+  last_edited: string;
+  start_time: string;
+  end_time: string;
+};

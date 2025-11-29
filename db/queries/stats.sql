14e0ed1d3d1d0bfbf3445ede95309146b0a72bc4
Jack Cordery <jack.redolf@gmail.com>
2025-11-29 20:23:15 +0000

    get monthly unpaid data

--- db/queries/stats.sql
+++ db/queries/stats.sql
@@ -357,3 +357,47 @@ SELECT
     FROM
       prev
   ) as delta;
+
+-- name: MonthlyUnpaidDelta :one
+with
+  curr as (
+    SELECT
+      b.id,
+      b.cost as cost
+    FROM
+      bookings as b
+      LEFT JOIN booking_slots as bs on b.id = bs.booking_id
+      LEFT JOIN availability as a on bs.availability_slot_id = a.id
+    WHERE
+      b.paid = false
+      and (a.datetime > now() - interval '30 day')
+    GROUP BY
+      b.id
+  ),
+  prev as (
+    SELECT
+      b.id,
+      b.cost as cost
+    FROM
+      bookings as b
+      LEFT JOIN booking_slots as bs on b.id = bs.booking_id
+      LEFT JOIN availability as a on bs.availability_slot_id = a.id
+    WHERE
+      b.paid = false
+      and (a.datetime < now() - interval '30 day')
+      and (a.datetime > now() - interval '60 day')
+    GROUP BY
+      b.id
+  )
+SELECT
+  (
+    SELECT
+      COALESCE(sum(cost), 0)
+    FROM
+      curr
+  ) - (
+    SELECT
+      COALESCE(sum(cost), 0)
+    FROM
+      prev
+  ) as delta;
